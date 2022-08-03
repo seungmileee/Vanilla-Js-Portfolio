550c1f654d7a3814ff59299ceb1a07904109d8c0
seungmileee <dltmdal0928@naver.com>
2022-08-03 22:29:55 +0900

    home transparent

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 "use strict";
 
+//스크롤 시 네비게이션 바 색 변경
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar_dark");
@@ -10,3 +10,34 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar_dark");
   }
 });
+
+// 네비게이션 바 버튼, 컨택 미 버튼 누를 때 해당 요소로 스크롤
+
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+const contactBtn = document.querySelector(".home__contact");
+
+contactBtn.addEventListener("click", (e) => {
+  scrollIntoView("#contact");
+});
+
+// 스크롤 내릴 때 홈 섹션 점점 더 투명해지도록 만들기
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
